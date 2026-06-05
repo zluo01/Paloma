c0816d3efc7dc09127ff33556517ec83d6c57765
zluo01 <12102011+zluo01@users.noreply.github.com>
2026-06-04 22:36:54 -0700

    Group storage SQL queries

--- crates/scry-storage/src/db/queries.rs
+++ crates/scry-storage/src/db/queries.rs
@@ -1,26 +1,28 @@
 pub const INIT_TABLE_QUERY: &str = include_str!("queries/init_table.sql");
 
-pub const INSERT_PROVIDER_QUERY: &str = include_str!("queries/insert_provider.sql");
+pub const INSERT_PROVIDER_QUERY: &str = include_str!("queries/providers/insert_provider.sql");
 
-pub const UPDATE_PROVIDER_QUERY: &str = include_str!("queries/update_provider.sql");
+pub const UPDATE_PROVIDER_QUERY: &str = include_str!("queries/providers/update_provider.sql");
 
 pub const UPDATE_PROVIDER_PREFERENCES_QUERY: &str =
-    include_str!("queries/update_provider_preferences.sql");
+    include_str!("queries/providers/update_provider_preferences.sql");
 
-pub const DELETE_PROVIDER_QUERY: &str = include_str!("queries/delete_provider.sql");
+pub const DELETE_PROVIDER_QUERY: &str = include_str!("queries/providers/delete_provider.sql");
 
-pub const CONNECTED_PROVIDERS_QUERY: &str = include_str!("queries/connected_providers.sql");
+pub const CONNECTED_PROVIDERS_QUERY: &str =
+    include_str!("queries/providers/connected_providers.sql");
 
-pub const CREATE_NEW_SESSION_QUERY: &str = include_str!("queries/create_new_session.sql");
+pub const CREATE_NEW_SESSION_QUERY: &str = include_str!("queries/sessions/create_new_session.sql");
 
-pub const TOUCH_SESSION_QUERY: &str = include_str!("queries/touch_session.sql");
+pub const TOUCH_SESSION_QUERY: &str = include_str!("queries/sessions/touch_session.sql");
 
-pub const GET_ALL_SESSIONS_QUERY: &str = include_str!("queries/get_all_sessions.sql");
+pub const GET_ALL_SESSIONS_QUERY: &str = include_str!("queries/sessions/get_all_sessions.sql");
 
-pub const DELETE_SESSION_QUERY: &str = include_str!("queries/delete_session.sql");
+pub const DELETE_SESSION_QUERY: &str = include_str!("queries/sessions/delete_session.sql");
 
-pub const PREFER_MODEL_CONFIG_QUERY: &str = include_str!("queries/prefer_model_config.sql");
+pub const PREFER_MODEL_CONFIG_QUERY: &str =
+    include_str!("queries/providers/prefer_model_config.sql");
 
-pub const MATCH_PERMISSION_QUERY: &str = include_str!("queries/match_permission.sql");
+pub const MATCH_PERMISSION_QUERY: &str = include_str!("queries/permissions/match_permission.sql");
 
-pub const INSERT_PERMISSION_QUERY: &str = include_str!("queries/insert_permission.sql");
+pub const INSERT_PERMISSION_QUERY: &str = include_str!("queries/permissions/insert_permission.sql");
