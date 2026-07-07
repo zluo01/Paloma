8e5ad6b13c7bd0eb61a0f5c0a1c5a2623595b47f
zluo01 <12102011+zluo01@users.noreply.github.com>
2026-07-06 18:07:03 -0700

    build scry-gtk as a dependency-free stub on non-linux

--- gui/scry-gtk/src/main.rs
+++ gui/scry-gtk/src/main.rs
@@ -1,12 +1,25 @@
+#[cfg(target_os = "linux")]
 mod app;
+#[cfg(target_os = "linux")]
 mod helper;
+#[cfg(target_os = "linux")]
 mod runtime;
+#[cfg(target_os = "linux")]
 mod services;
+#[cfg(target_os = "linux")]
 mod style;
+#[cfg(target_os = "linux")]
 mod widgets;
 
 use std::process::ExitCode;
 
+#[cfg(target_os = "linux")]
 fn main() -> ExitCode {
     app::run()
+}
+
+#[cfg(not(target_os = "linux"))]
+fn main() -> ExitCode {
+    eprintln!("scry (GTK frontend) only supports Linux");
+    ExitCode::FAILURE
 }
