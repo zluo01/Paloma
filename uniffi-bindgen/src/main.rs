2377104cdfd61a94e8de0ed493b3964047fee4a3
zluo01 <12102011+zluo01@users.noreply.github.com>
2026-07-08 22:59:26 -0700

    only build bindgen in macos.

--- uniffi-bindgen/src/main.rs
+++ uniffi-bindgen/src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(target_os = "macos")]
 fn main() {
     uniffi::uniffi_bindgen_main()
+}
+
+#[cfg(not(target_os = "macos"))]
+fn main() -> std::process::ExitCode {
+    eprintln!("uniffi-bindgen is only used for the macos frontend");
+    std::process::ExitCode::FAILURE
 }
--- uniffi-bindgen/src/swift.rs
+++ uniffi-bindgen/src/swift.rs
@@ -1,3 +1,10 @@
+#[cfg(target_os = "macos")]
 fn main() {
     uniffi::uniffi_bindgen_swift()
+}
+
+#[cfg(not(target_os = "macos"))]
+fn main() -> std::process::ExitCode {
+    eprintln!("uniffi-bindgen-swift is only used for the macos frontend");
+    std::process::ExitCode::FAILURE
 }
