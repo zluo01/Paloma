4c4f549ab0bbfac33c1ca4006a649ac93f72c705
zluo01 <12102011+zluo01@users.noreply.github.com>
2026-08-30 02:46:34 -0700

    fix clippy warning on linux.

--- core/src/utils/process.rs
+++ core/src/utils/process.rs
@@ -1,7 +1,9 @@
 use tokio::process::Command;
 
-/// make sure on windows we do not get popup terminal windows
+/// make sure on Windows we do not get popup terminal windows
 pub(crate) fn hide_console(cmd: &mut Command) {
     #[cfg(windows)]
     cmd.creation_flags(windows_sys::Win32::System::Threading::CREATE_NO_WINDOW);
+    #[cfg(not(windows))]
+    let _ = cmd;
 }
